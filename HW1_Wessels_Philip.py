d1878260e1b26b72686696b46bd7073479f4c90d
Ronnie Nigash <nigash@uw.edu>
2016-10-04 08:12:12 -0700

    Update rate constant to degrade protein

--- HW1_Wessels_Philip.py
+++ HW1_Wessels_Philip.py
@@ -53,16 +53,16 @@ modelstring2 = '''
                 // Protein degrades every hour (in seconds) --> add this to every model (line)
                 // use rate constant to apply this to models instead of event
 
-                k7 = 1/10; k8 = 1/(60*60);
-                J10: -> S3; k7 * S2 * k8
-                J11: -> S4; k7 * S2 * k8
-                J12: -> S5; k7 * S2 * k8
-                J13: -> S6; k7 * S2 * k8
-                J14: -> S7; k7 * S2 * k8
-                J15: -> S8; k7 * S2 * k8
-                J16: -> S9; k7 * S2 * k8
-                J17: -> S10; k7 * S2 * k8
-                J18: -> S11; k7 * S2 * k8
+                k7 = 1/(60*60);
+                J10: -> S3; k7 * S2
+                J11: -> S4; k7 * S2
+                J12: -> S5; k7 * S2
+                J13: -> S6; k7 * S2
+                J14: -> S7; k7 * S2
+                J15: -> S8; k7 * S2
+                J16: -> S9; k7 * S2
+                J17: -> S10; k7 * S2
+                J18: -> S11; k7 * S2
              ''' 
 
 modelstring2 += modelstring1_body_info
