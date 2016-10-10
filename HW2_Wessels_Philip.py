786a2156622eee7e6ceca27bcc0c6d96d0949a0c
Pearl Philip <pearlphilip007@gmail.com>
2016-10-10 11:41:13 -0700

    Added solution to question 2d

--- HW2_Wessels_Philip.py
+++ HW2_Wessels_Philip.py
@@ -4,7 +4,10 @@ Created on Sat Oct 08 16:42:17 2016
 
 @author: pearl
 """
+import numpy as np
 import tellurium as te
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 
 ### Question 1b
@@ -86,3 +89,23 @@ modelstring1c3 =  '''
 r4 = te.loada(modelstring1c3);
 model4 = r4.simulate(0, 10, 1000);
 r4.plot(model4);
+
+### Question 2d
+def ode(X, t):
+    x, y = X
+    dXdt = [-0.5 * (x ** 2) * y,  0.5 * (x ** 2) * y]
+    return dXdt
+
+X0 = [1, 2]
+t = np.linspace(0, 10, 101)
+sol = odeint(ode, X0, t) 
+plt.plot(t, sol[:, 0], 'b', label='x(t)')
+plt.plot(t, sol[:, 1], 'g', label='y(t)')
+plt.legend(loc='best')
+plt.xlabel('Time t')
+plt.ylabel('x and y concentrations')
+plt.title('Dynamics of reaction system with [x] = 1 and [y] = 2 at t = 0')
+plt.grid()
+plt.show()
+
+
