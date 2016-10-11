1f01d0535ca0aa964f0c6f9d64d19e446fcf3ef4
Pearl Philip <pearlphilip007@gmail.com>
2016-10-11 13:07:52 -0700

    Added correct code for hw1 q2. To be modified for use in hw2 q3

--- HW2_Wessels_Philip.py
+++ HW2_Wessels_Philip.py
@@ -5,6 +5,8 @@ Created on Sat Oct 08 16:42:17 2016
 @author: pearl
 """
 import numpy as np
+import os
+import pandas as pd
 import tellurium as te
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
@@ -28,7 +30,7 @@ modelstring1b =  '''
 
 r1 = te.loada(modelstring1b);
 model1 = r1.simulate(0, 10, 1000);
-r1.plot(model1);
+r1.plot(model1, title="Dynamics of A, B & C when k1 = 1, k2 = 1, k3 = 1");
 
 ### Question 1c, Trial 1
 modelstring1c1 =  '''
@@ -48,7 +50,7 @@ modelstring1c1 =  '''
 
 r2 = te.loada(modelstring1c1);
 model2 = r2.simulate(0, 10, 1000);
-r2.plot(model2);
+r2.plot(model2, title="Dynamics of A, B & C when k1 = 2, k2 = 2, k3 = 2");
 
 ### Question 1c, Trial 2
 modelstring1c2 =  '''
@@ -68,7 +70,7 @@ modelstring1c2 =  '''
 
 r3 = te.loada(modelstring1c2);
 model3 = r3.simulate(0, 10, 1000);
-r3.plot(model3);
+r3.plot(model3, title="Dynamics of A, B & C when k1 = 2, k2 = 1, k3 = 1");
 
 ### Question 1c, Trial 3
 modelstring1c3 =  '''
@@ -88,7 +90,7 @@ modelstring1c3 =  '''
 
 r4 = te.loada(modelstring1c3);
 model4 = r4.simulate(0, 10, 1000);
-r4.plot(model4);
+r4.plot(model4, title="Dynamics of A, B & C when k1 = 2, k2 = 3, k3 = 5");
 
 ### Question 2d
 def ode(X, t):
@@ -108,4 +110,35 @@ plt.title('Dynamics of reaction system with [x] = 1 and [y] = 2 at t = 0')
 plt.grid()
 plt.show()
 
+### Question 3
+modelstring3 = '''
+                R1: -> RNA; k1;
+                R2: -> Protein; k2 * RNA;
+ 
+                #variables that get set by python code
+                RNA = 0; Protein = 0; k1 = 0; k2 = 0;
+                
+                '''
+r3 = te.loada(modelstring3)
+# Reciprocals of the rate constants
+reciprocal_k1 = [10, 100, 1000]
+reciprocal_k2 = [10, 20, 30]
 
+for i in reciprocal_k1:
+    for j in reciprocal_k2:
+        r3.RNA = 0
+        r3.Protein = 0
+        r3.k1 = 1.0/i
+        r3.k2 = 1.0/j
+        
+        model3 = r3.simulate(0, 18 * 60 * 60, 1000) 
+        # End time: 18 hr * 60 min/hr * 60 s/min
+        r3.plot(model3, title="k1 = every %is k2 = every %is"%(i, j))
+        
+        # Saving the model into a csv file
+        df_model3 = pd.DataFrame(model3)
+        df_model3.columns = ['Time(s)', 'RNA', 'Protein']
+        output_filename = 'k1_%is_k2_%is.csv'%(i,j)
+        output_path = os.path.join('./csv_files', output_filename)
+        df_model3.to_csv(output_path)
+        
