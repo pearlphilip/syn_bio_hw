1053d2e9920314be274540822461e6b0e61f729e
Ronnie Nigash <nigash@uw.edu>
2016-11-03 21:54:15 -0700

    Update Model 4
    Init model 4 code

--- HW4_Wessels_Philip.py
+++ HW4_Wessels_Philip.py
@@ -13,27 +13,27 @@ r2 = te.loada('''
                 R1: => RNA_tetR; k1;
                 R2: => RNA_lacI; k2;
                 R3: => RNA_glk; k3;
-                
+
                 k1 = 10; k2 = 10; k3 = 10;
-                
-                # Translation                
+
+                # Translation
                 R4: RNA_lacI => lacI; k4 * RNA_lacI;
                 R5: RNA_glk => glk; k5 * RNA_glk;
-                
+
                 k4 = 10; k5 = 10;
-            
+
                 # Degradation, dilution and cell division
                 R6: RNA_tetR => ; k_cell_div
                 R7: RNA_lacI => ; k_cell_div
                 R8: RNA_tetR => ; k_cell_div
                 R9: lacI => ; k_cell_div
-                R10: glk => ; k_cell_div 
- 
+                R10: glk => ; k_cell_div
+
                 #variables that get set by python code
                 RNA_tetR = 0; RNA_lacI = 0; RNA_glk = 0;
-                tetR = 0; lacI = 0; glk = 0; 
-                
-                # Dilution occurs as the cell divides, therefore k_cell_div = 
+                tetR = 0; lacI = 0; glk = 0;
+
+                # Dilution occurs as the cell divides, therefore k_cell_div =
                 # k_dilution and is calculated from standard cell doubling
                 # time
                 k_cell_div = 1.28 *(10 ^(-4)); k_dilution = 1.28 *(10 ^(-4));
@@ -50,27 +50,27 @@ modelstring3 = ('''
                 R1: => RNA_tetR; k1;
                 R2: => RNA_lacI; k2;
                 R3: => RNA_glk; k3;
-                
+
                 k1 = 10; k2 = 10; k3 = 10;
-                
-                # Translation                
+
+                # Translation
                 R4: RNA_lacI => lacI; k4 * RNA_lacI;
                 R5: RNA_glk => glk; k5 * RNA_glk;
-                
+
                 k4 = 10; k5 = 10;
-            
+
                 # Degradation, dilution and cell division
                 R6: RNA_tetR => ; k_cell_div
                 R7: RNA_lacI => ; k_cell_div
                 R8: RNA_tetR => ; k_cell_div
                 R9: lacI => ; k_cell_div
-                R10: glk => ; k_cell_div 
- 
+                R10: glk => ; k_cell_div
+
                 #variables that get set by python code
                 RNA_tetR = 0; RNA_lacI = 0; RNA_glk = 0;
-                tetR = 0; lacI = 0; glk = 0; 
-                
-                # Dilution occurs as the cell divides, therefore k_cell_div = 
+                tetR = 0; lacI = 0; glk = 0;
+
+                # Dilution occurs as the cell divides, therefore k_cell_div =
                 # k_dilution and is calculated from standard cell doubling
                 # time
                 k_cell_div = 1.28 *(10 ^(-4)); k_dilution = 1.28 *(10 ^(-4));
@@ -80,3 +80,51 @@ r3 = te.loada(modelstring3)
 r3.timeCourseSelections = ['time', '[glk]']
 model3 = r3.simulate(0, 400, steps=1000)
 r3.plot(model3, title="glk trend in a system without aTc")
+
+
+# Question 4
+modelstring4 = ('''
+                # Transcription
+                R2: DNA_lacI => DNA_lacI + RNA_lacI; k2 * DNA_lacI;
+                R3: DNA_glk => RNA_glk + DNA_glk; k3 * DNA_glk;
+                R13: DNA_tetR => RNA_tetR + DNA_tetR; k13 * DNA_tetR
+
+                k1 = 10; k2 = 10; k3 = 10;
+
+                # Translation
+                R4: RNA_lacI => lacI; k4 * RNA_lacI;
+                R5: RNA_glk => glk; k5 * RNA_glk;
+                # Want to measure glk; plot of y axis
+
+                k4 = 10; k5 = 10;
+
+                # Inhibition
+                R11: lacI + DNA_glk => lacI_DNA_glk_complex; k11 * lacI * DNA_glk;
+                R12: lacI_DNA_glk_complex => DNA_glk; k12 * lacI_DNA_glk_complex;
+                R14: RNA_tetR + DNA_lacI => RNA_tetR_DNA_lacI_complex; k14 * RNA_tetR * DNA_lacI;
+                R15: RNA_tetR_DNA_lacI_complex => DNA_lacI; k15 * RNA_tetR_DNA_lacI_complex;
+                R16: aTc + RNA_tetR => aTc_RNA_tetR_complex; k16 * aTc * RNA_tetR;
+                R17: aTc_RNA_tetR_complex => RNA_tetR; k17 * aTc;
+                # k12 is the degredation rate of lacI; vary this and plot on x axis
+
+                # Degradation, dilution and cell division
+                R7: RNA_lacI => ; k_cell_div
+                R8: RNA_glk => ; k_cell_div
+                R9: lacI => ; k_cell_div
+                R10: glk => ; k_cell_div
+                R18: RNA_tetR; k_cell_div
+
+                #variables that get set by python code
+                RNA_tetR = 0; RNA_lacI = 0; RNA_glk = 0;
+                tetR = 0; lacI = 0; glk = 0; DNA_lacI = 1; DNA gk = 1; DNA_tetR = 1;
+
+                # Dilution occurs as the cell divides, therefore k_cell_div =
+                # k_dilution and is calculated from standard cell doubling
+                # time
+                k_cell_div = 1.28 *(10 ^(-4)); k_dilution = 1.28 *(10 ^(-4));
+                ''')
+
+r4 = te.loada(modelstring4)
+r4.timeCourseSelections = ['time', '[glk]']
+model4 = r4.simulate(0, 400, steps=1000)
+r4.plot(model4, title="Level of glk enzyme as a function of lacI degradation rate")
